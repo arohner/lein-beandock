04a2844bdce935c7dc394ec7780d78f4eae56f6e
Allen Rohner <arohner@gmail.com>
2015-04-07 16:21:11 -0500

    Add description, url, remove unnecessary tools.reader dep

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject lein-beandock "0.1.2"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Tools for deploying docker containers to AWS ElasticBeandock"
+  :url "https://github.com/arohner/lein-beandock"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.amazonaws/aws-java-sdk "1.3.31"]
                  [cheshire "5.4.0"]
                  [lein-beanstalk "0.2.7"]
                  [arohner/lein-docker "0.1.0"]
-                 [amazonica "0.3.19"]
-                 [org.clojure/tools.reader "0.8.16"]]
+                 [amazonica "0.3.19"]]
   :eval-in-leiningen true)
